ba8b9ab55b1b3fd6e3aa507928ce21e808f10ac9
Denis <den.zagrosh@gmail.com>
2023-06-19 01:16:27 +0300

    development #

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import './styles/index.scss';
 export const App = () => {
     return (
         <div className='app'>
+            <div className={'DEVELOPMENT'}>IN DEVELOPMENT</div>
             <Navbar />
             <AppRouter />
         </div>
